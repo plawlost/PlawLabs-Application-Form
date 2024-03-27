cb00cb3bf3d3774db9dc9af6c75c4b7cfb23d58a
Yağız Erkam "Plawlost" Çelebi <84669529+plawlost@users.noreply.github.com>
2024-03-27 13:17:21 +0300

    Update run.py

--- run.py
+++ run.py
@@ -2,7 +2,9 @@
 from set_data import save_application_data
 from initialize import initialize_page
 from application import application_form
-from styling import apply_custom_cs
+from styling import apply_custom_css
+
+# FILE EXECUTION
 from variables import variables_define
 
 # OTHER MODULES
@@ -12,18 +14,10 @@ import json
 from pathlib import Path
 from streamlit_ace import st_ace
 
-# LAST EDITS \\ CHANGE THIS IF YOU SEE
-def deletethis():
-  """Delete this function."""
-  if st.session_state.page_number > 1 and st.session_state.page_number < 15:
-    if st.button('Previous Section'):
-      st.session_state.page_number -= 1
-    if st.button('Next Section'):
-      st.session_state.page_number += 1
-  
- # RUNNING FUNCTION
- def execute():
-     initialize_page()
-     application_form()
-     apply_custom_css()
-     deletethis()
+
+# RUNNING FUNCTION
+def execute():
+  """Executes the application."""
+  initialize_page()
+  application_form()
+  apply_custom_css()
